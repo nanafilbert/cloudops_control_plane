4409be4fe929e207ca0a2d2e6bf2efb4e0046744
nanafilbert <filbertnanablessing@gmail.com>
2026-04-23 09:44:02 +0000

    changed environments to env to tally workflow

--- terraform/environments/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloudops-tfstate-eaf05ade5d"  # Replace with actual bucket name from bootstrap output
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "cloudops-terraform-lock"
-  }
-}
