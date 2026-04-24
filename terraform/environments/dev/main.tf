00650df2c6da83c32a7388bba50d9ccc554a9f5d
nanafilbert <filbertnanablessing@gmail.com>
2026-04-24 07:55:58 +0000

    fix: use free-tier eligible t4g.micro nodes for EKS

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -32,7 +32,7 @@ module "eks" {
   source = "../../modules/eks"
 
   cluster_name        = "cloudops-dev"
-  cluster_version     = "1.29"
+  cluster_version     = "1.30"
   vpc_id              = module.vpc.vpc_id
   private_subnet_ids  = module.vpc.private_subnet_ids
 
@@ -41,7 +41,7 @@ module "eks" {
       desired_size = 1
       min_size     = 1
       max_size     = 2
-      instance_types = ["t3.medium"]
+      instance_types = ["t4g.micro"]
       ami_type = "AL2_x86_64"
     }
   }
