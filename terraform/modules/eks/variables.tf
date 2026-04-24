414e1801f9596d5a55b7412833e559864ec1e395
nanafilbert <filbertnanablessing@gmail.com>
2026-04-24 10:34:07 +0000

    fix: remove unsupported aws-auth arguments from EKS module

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -30,18 +30,3 @@ variable "tags" {
   default     = {}
 }
 
-variable "manage_aws_auth_configmap" {
-  description = "Whether to manage the aws-auth ConfigMap"
-  type        = bool
-  default     = false
-}
-
-variable "aws_auth_roles" {
-  description = "List of roles to add to aws-auth ConfigMap"
-  type = list(object({
-    rolearn  = string
-    username = string
-    groups   = list(string)
-  }))
-  default = []
-}
