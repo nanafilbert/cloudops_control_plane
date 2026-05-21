0288273888cbfd9241daa356ac7ca4c978d6e6e6
nanafilbert <filbertnanablessing@gmail.com>
2026-05-21 18:29:20 +0000

    fix: Force delete pending secrets

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -112,3 +112,19 @@ resource "aws_iam_openid_connect_provider" "eks" {
 
   tags = merge(var.tags, { Name = "${var.cluster_name}-oidc-provider" })
 }
+
+resource "aws_eks_access_entry" "admin_user" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = "arn:aws:iam::234506497205:user/filbert" 
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "admin_user" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = aws_eks_access_entry.admin_user.principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
